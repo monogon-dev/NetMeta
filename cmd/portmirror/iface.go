78a9eb9e71efb7600f21d9bf5504a5222762a343
Fionera <fionera@fionera.de>
2021-11-18 02:09:17 +0100

    feat: Replace SysFS dependency, Set promisc mode to on, Use multiple goroutines for the socket
    Change-Id: I0fe8f8602a4657dd03d89ffeee492617463dc99e
    Reviewed-on: https://review.monogon.dev/c/NetMeta/+/954
    Reviewed-by: Leopold Schabel <leo@monogon.tech>

--- cmd/portmirror/iface.go
+++ cmd/portmirror/iface.go
@@ -5,21 +5,22 @@ import (
 	"net"
 	"strings"
 	"sync"
+	"sync/atomic"
 
 	flowprotob "github.com/cloudflare/goflow/v3/pb"
 	"github.com/cloudflare/goflow/v3/transport"
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/afpacket"
 	"github.com/google/gopacket/layers"
-	"github.com/prometheus/procfs/sysfs"
 	"github.com/sirupsen/logrus"
+	"github.com/vishvananda/netlink"
 )
 
 type tapInterface struct {
-	name     string
-	sysIface sysfs.NetClassIface
-	packet   *afpacket.TPacket
-	pair     *tapPair
+	name    string
+	ifIndex int
+	isRX    bool
+	counter uint64
 }
 
 type tapPair struct {
@@ -28,74 +29,53 @@ type tapPair struct {
 	TX             *tapInterface
 }
 
-type netClasses map[string]sysfs.NetClassIface
-
-func newNetClasses() (netClasses, error) {
-	fs, err := sysfs.NewFS("/sys")
-	if err != nil {
-		return nil, err
-	}
-
-	class, err := fs.NewNetClass()
-	if err != nil {
-		return nil, err
-	}
-
-	ifaceClasses := make(netClasses)
-	for _, c := range class {
-		ifaceClasses[c.Name] = c
-	}
-
-	return ifaceClasses, nil
-}
-
-func (nc netClasses) getNetClass(name string) sysfs.NetClassIface {
-	class, ok := nc[name]
-	if !ok {
-		logrus.Fatalf("cant find interface: %q", name)
-	}
-	return class
-}
-
 func loadConfig() []*tapPair {
-	if len(*Interfaces) == 0 {
+	if len(interfaces) == 0 {
 		logrus.Fatal("please provide interface pairs")
 	}
 
-	samplerAddress := net.ParseIP(*SamplerAddress)
-	if samplerAddress == nil {
-		logrus.Fatalf("invalid sampler-address provided: %q", *SamplerAddress)
-	}
-
-	netClasses, err := newNetClasses()
-	if err != nil {
-		logrus.Fatalf("loading net class info: %v", err)
-	}
-
 	var tapPairs []*tapPair
-	for _, v := range strings.Split(*Interfaces, ",") {
+	for _, v := range strings.Split(interfaces, ",") {
 		if !strings.Contains(v, ":") {
 			logrus.Fatalf("invalid interface pair given: %q", v)
 		}
 
 		split := strings.Split(v, ":")
+		if len(split) != 2 {
+			logrus.Fatal("invalid interface pair given: %q", v)
+		}
+
 		rxName, txName := split[0], split[1]
+		rxLink, err := netlink.LinkByName(rxName)
+		if err != nil {
+			logrus.Fatalf("cant find interface: %q", rxName)
+		}
+
+		if err := netlink.SetPromiscOn(rxLink); err != nil {
+			logrus.Fatalf("cant set promisc mode on interface: %q", rxName)
+		}
+
+		txLink, err := netlink.LinkByName(txName)
+		if err != nil {
+			logrus.Fatalf("cant find interface: %q", txName)
+		}
+
+		if err := netlink.SetPromiscOn(txLink); err != nil {
+			logrus.Fatalf("cant set promisc mode on interface: %q", txName)
+		}
+
 		t := &tapPair{
 			samplerAddress: samplerAddress,
 			RX: &tapInterface{
-				name:     rxName,
-				sysIface: netClasses.getNetClass(rxName),
+				name:    rxName,
+				isRX:    true,
+				ifIndex: rxLink.Attrs().Index,
 			},
 			TX: &tapInterface{
-				name:     txName,
-				sysIface: netClasses.getNetClass(txName),
+				name:    txName,
+				ifIndex: txLink.Attrs().Index,
 			},
 		}
-		t.RX.pair = t
-		t.TX.pair = t
-
-		logrus.Printf("Interface %q: %d", t.RX.sysIface.Name, *t.RX.sysIface.IfIndex)
-		logrus.Printf("Interface %q: %d", t.TX.sysIface.Name, *t.TX.sysIface.IfIndex)
 
 		tapPairs = append(tapPairs, t)
 	}
@@ -103,65 +83,56 @@ func loadConfig() []*tapPair {
 	return tapPairs
 }
 
-func (ti *tapInterface) Open() error {
-	tPacket, err := afpacket.NewTPacket(
+func (ti *tapInterface) Worker(ctx context.Context, startGroup *sync.WaitGroup, endGroup *sync.WaitGroup, kafkaState *transport.KafkaState) {
+	defer endGroup.Done()
+
+	handle, err := afpacket.NewTPacket(
 		afpacket.OptInterface(ti.name),
 		afpacket.SocketRaw,
-		afpacket.TPacketVersion3)
+		afpacket.TPacketVersion3,
+	)
 	if err != nil {
-		return err
+		logrus.Fatalf("opening interface %q: %v", ti.name, err)
+	}
+	err = handle.SetFanout(afpacket.FanoutLoadBalance, uint16(fanoutBase*ti.ifIndex))
+	if err != nil {
+		logrus.Fatalf("setting fanout on interface %q: %v", ti.name, err)
+	}
+	defer handle.Close()
+
+	startGroup.Done()
+	startGroup.Wait()
+
+	var flowDirection uint32
+	var inIf, outIf uint32
+	if ti.isRX {
+		flowDirection = 0 // Inbound traffic because this is a RX Mirror interface
+		inIf = uint32(ti.ifIndex)
+	} else {
+		flowDirection = 1 // Outbound traffic because this is a TX Mirror interface
+		outIf = uint32(ti.ifIndex)
 	}
 
-	if err := tPacket.InitSocketStats(); err != nil {
-		return err
-	}
-
-	ti.packet = tPacket
-	return nil
-}
-
-func (ti *tapInterface) Run(ctx context.Context, wg *sync.WaitGroup, kafkaState *transport.KafkaState) {
-	defer wg.Done()
-	defer func() {
-		ti.packet.Close()
-		logrus.Printf("Stopping Interface %q", ti.name)
-	}()
-
-	for seqNum := 0; true; seqNum++ {
+	for {
 		select {
 		case <-ctx.Done():
 			return
 		default:
 		}
 
-		data, ci, err := ti.packet.ZeroCopyReadPacketData()
-		if err != nil {
-			logrus.Fatal(err)
-		}
-
-		if seqNum%*SampleRate != 0 {
+		data, ci, _ := handle.ZeroCopyReadPacketData()
+		if atomic.AddUint64(&ti.counter, 1)%uint64(sampleRate) != 0 {
 			continue
-		}
-
-		isRX := ti.pair.RX == ti
-		var flowDirection uint32
-		var inIf, outIf uint32
-		if isRX {
-			flowDirection = 0 // Inbound traffic because this is a RX Mirror interface
-			inIf = uint32(*ti.sysIface.IfIndex)
-		} else {
-			flowDirection = 1 // Outbound traffic because this is a TX Mirror interface
-			outIf = uint32(*ti.sysIface.IfIndex)
 		}
 
 		info := readFrameInfo(gopacket.NewPacket(data, layers.LayerTypeEthernet, gopacket.Default))
 		msg := &flowprotob.FlowMessage{
 			Type:           flowprotob.FlowMessage_SFLOW_5,
 			TimeReceived:   uint64(ci.Timestamp.Unix()),
-			SequenceNum:    uint32(seqNum),
-			SamplingRate:   uint64(*SampleRate),
+			SequenceNum:    info.SeqNum,
+			SamplingRate:   uint64(sampleRate),
 			FlowDirection:  flowDirection,
-			SamplerAddress: ti.pair.samplerAddress,
+			SamplerAddress: samplerAddress,
 			TimeFlowStart:  uint64(ci.Timestamp.Unix()),
 			TimeFlowEnd:    uint64(ci.Timestamp.Unix()),
 			Bytes:          uint64(ci.Length),
--- go.mod
+++ go.mod
@@ -7,8 +7,8 @@ require (
 	github.com/golang-migrate/migrate/v4 v4.10.0
 	github.com/google/gopacket v1.1.19
 	github.com/osrg/gobgp v0.0.0-20201220152754-839495d33041
-	github.com/prometheus/procfs v0.0.0-20190117184657-bf6a532e95b1
 	github.com/sirupsen/logrus v1.4.2
+	github.com/vishvananda/netlink v0.0.0-20170802012344-a95659537721
 	google.golang.org/protobuf v1.25.0 // indirect
 	k8s.io/klog/v2 v2.4.0
 )
